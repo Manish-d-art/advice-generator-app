6ea5e1e2c1cd5db6aa8c03e71d11c43402f8cf1b
manish mandal <76776194mm@gmail.com>
2022-10-10 19:14:32 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -3,22 +3,27 @@ const Btn=document.querySelector('.main__diceContainer');
 const adviceId=document.querySelector('.main__advice-no');
 const advicePara=document.querySelector('.main__adviceText');
 
-fetch('https://api.adviceslip.com/advice')
-.then(response => {
-  // console.log(response.json());
-  if(! response.ok)
-    throw new Error(`advice not found(${response.status})`);
-  return response.json();
-})
-.then(data =>{
-  console.log(data);
-  const id=data.slip.id;
-  let adviceText=data.slip.advice;
-  advicePara.textContent=adviceText;
-  console.log(adviceText);
-  console.log(id);
-  adviceId.textContent=id;
-})
-.catch(error =>{
-  console.log(2);
-});
+const adviceFunc=function(){
+  fetch('https://api.adviceslip.com/advice')
+  .then(response => {
+    if(! response.ok)
+      throw new Error(`advice not found(${response.status})`);
+    return response.json();
+  })
+  .then(data =>{
+    console.log(data);
+    const id=data.slip.id;
+    let adviceText=data.slip.advice;
+    advicePara.textContent=adviceText;
+    console.log(adviceText);
+    console.log(id);
+    adviceId.textContent=id;
+  })
+  .catch(error =>{
+    console.log(2);
+  });
+}
+
+
+Btn.addEventListener('click',adviceFunc);
+window.addEventListener('load',adviceFunc);
