5df02d33f5a3d185dc08af6cee88830326fd8f2a
manish mandal <76776194mm@gmail.com>
2022-10-10 18:24:17 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const Btn=document.querySelector('.main__diceContainer');
 
 fetch('https://api.adviceslip.com')
 .then(response => {
+  console.log(response);
   if(! response.ok)
     throw new Error(`advice not found(${response.status})`);
-    return response.json();
-});
+  return response.json();
+})
+.then(data =>{
+  console.log(data);
+})
+.catch(error =>{
+  console.log(2);
+})
