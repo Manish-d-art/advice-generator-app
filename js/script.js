06ce2ed381bbb0da5fc6fbd343b4e8f6c4addfa2
manish mandal <76776194mm@gmail.com>
2022-10-10 19:04:04 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 const Btn=document.querySelector('.main__diceContainer');
 const adviceId=document.querySelector('.main__advice-no');
+const advicePara=document.querySelector('.main__adviceText');
 
 fetch('https://api.adviceslip.com/advice')
 .then(response => {
@@ -12,10 +13,12 @@ fetch('https://api.adviceslip.com/advice')
 .then(data =>{
   console.log(data);
   const id=data.slip.id;
-  const adviceText=data.slip.advice;
-  console.log(id);
+  let adviceText=data.slip.advice;
+  advicePara.textContent=adviceText;
   console.log(adviceText);
+  console.log(id);
+  adviceId.textContent=id;
 })
-// .catch(error =>{
-//   console.log(2);
-// });
+.catch(error =>{
+  console.log(2);
+});
